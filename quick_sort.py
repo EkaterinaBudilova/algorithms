5e690d5016c050aebbc1cd8054b623b29be0259b
Ekaterina <ekatbdl@gmail.com>
2023-06-05 17:31:27 +1200

    iterations counter added and class name changed

--- quick_sort.py
+++ quick_sort.py
@@ -2,10 +2,11 @@ import timeit
 import numpy as np
 
 
-class FastSort:
+class QuickSort:
 
     def __init__(self, arr) -> None:
         self.arr = arr
+        self.__iterations = 0
 
     @staticmethod
     def sort_elements(arr: list) -> list:
@@ -19,12 +20,19 @@ class FastSort:
             less_elements = [i for i in arr[1:] if i <= base_element]
             greater_elements = [i for i in arr[1:] if i > base_element]
 
-            return FastSort.sort_elements(less_elements) + [base_element] + FastSort.sort_elements(greater_elements)
+            return QuickSort.sort_elements(less_elements) + [base_element] + QuickSort.sort_elements(greater_elements)
 
     def call_sort(self) -> list:
         """Calls sorting method"""
-        return FastSort.sort_elements(self.arr)
+
+        self.__iterations += 1
+        return QuickSort.sort_elements(self.arr)
+
+    @property
+    def iterations(self):
+        return self.iterations
+
 
 if __name__ == "__main__":
-    s = FastSort(np.random.rand(10000))
+    s = QuickSort(np.random.rand(10000))
     print(timeit.timeit("s.call_sort()", setup="from __main__ import s", number=100))
